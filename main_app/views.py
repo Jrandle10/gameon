7c81ccdf1b508214b47ab358520993fcda0499db
Jrandle10 <91491549+Jrandle10@users.noreply.github.com>
2021-11-27 01:30:44 -0700

    connect model to admin page

--- main_app/views.py
+++ main_app/views.py
@@ -1,19 +1,8 @@
 from django.shortcuts import render
-
+from .models import Game
 
 # Create your views here.
-class Game:
-  def __init__(self, name, platform, description, release_date):
-    self.name = name
-    self.platform = platform
-    self.description = description
-    self.release_date = release_date
 
-games = [
-  Game('God Of War', 'Playstation', 'Kranos takes down all the gods', '2005'),
-  Game('Madden 21', 'Xbox', 'NFL game', '2020'),
-  Game('Fifa 21', 'Playstation', 'MLB soccer game', '2020'),
-]
 
 def home(request):
   return render(request, 'home.html')
@@ -22,4 +11,5 @@ def about(request):
   return render(request, 'about.html')
 
 def games_index(request):
+  games = Game.objects.all()
   return render(request, 'games/index.html', {'games': games})
