2f5a69b479d34d1429ce64d2abea2b20bf821231
Jrandle10 <91491549+Jrandle10@users.noreply.github.com>
2021-11-29 13:22:00 -0700

    Config deployment

--- gameon/settings.py
+++ gameon/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('pl8auq@cz+4gz*z-*yix=w2x$c3dvl%2brfopvia(wp)say5&+')
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG') == 'True'
