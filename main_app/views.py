b7b2a7c11ba61d62eda17f6486e81c53143dcc1a
Jrandle10 <91491549+Jrandle10@users.noreply.github.com>
2021-11-28 22:07:23 -0700

    add auth to app

--- main_app/views.py
+++ main_app/views.py
@@ -1,33 +1,59 @@
 from django.db.models import fields
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Game
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-def home(request):
-  return render(request, 'home.html')
+class Home(LoginView):
+  template_name = 'home.html'
 
 def about(request):
   return render(request, 'about.html')
 
+@login_required
 def games_index(request):
-  games = Game.objects.all()
+  games = Game.objects.filter(user=request.user)
   return render(request, 'games/index.html', {'games': games })
 
+@login_required
 def games_detail(request, game_id):
   game = Game.objects.get(id=game_id)
   return render(request, 'games/detail.html', { 'game': game })
 
-class GameCreate(CreateView):
+class GameCreate(LoginRequiredMixin, CreateView):
   model = Game
   fields = '__all__'
   success_url = '/games/'
 
-class GameUpdate(UpdateView):
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form)
+
+class GameUpdate(LoginRequiredMixin, UpdateView):
   model = Game
   fields = ['platform', 'description']
 
 class GameDelete(DeleteView):
   model = Game
   success_url = '/games/'
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('games_index')
+    else:
+      error_message = 'Invalid sign up - try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'signup.html', context)
+
